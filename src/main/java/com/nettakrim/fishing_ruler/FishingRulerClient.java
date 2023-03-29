41a98aeec6cc85d98508685f86dac3d956975a93
Nettakrim <you@example.com>
2023-03-29 20:54:18 +0100

    added different colors for different situations

--- src/main/java/com/nettakrim/fishing_ruler/FishingRulerClient.java
+++ src/main/java/com/nettakrim/fishing_ruler/FishingRulerClient.java
@@ -1,6 +1,7 @@
 package com.nettakrim.fishing_ruler;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
@@ -15,28 +16,96 @@ public class FishingRulerClient implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Fishing Ruler");
 
 	public static MinecraftClient client;
+	private static int framesSinceUpdate = 8;
+	private static double lastLength = 0;
 
 	@Override
 	public void onInitializeClient() {
 		client = MinecraftClient.getInstance();
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {update();});
 	}
 
-	public static void updateText(double length) {
+	public static void update() {
+		//throwing the line straight down can cause the snapped indicator to not show up
+		if (framesSinceUpdate == 1 && lastLength > 31 && lastLength <= 32) {
+			updateText(32.1D, false, State.DEFAULT);
+		}
+		if (framesSinceUpdate < 8) {
+			framesSinceUpdate++;
+		}
+	}
+
+	public static void updateText(double length, boolean updateLast, State state) {
 		MutableText text;
-		if (length > 32D) {
-			text = Text.translatable(MODID+".snapped");
+		if (length > 32) {
+			text = Text.translatable(MODID+".snapped", formatDecimalPlaces(length));
+			framesSinceUpdate = 8;
 		} else {
 			text = Text.translatable(MODID+".length", formatDecimalPlaces(length));
 		}
-		if (length > 30) {
+
+		if (length > 32) {
+			text.setStyle(Style.EMPTY.withColor(Formatting.DARK_RED));
+		} else if (length > 30) {
 			text.setStyle(Style.EMPTY.withColor(Formatting.RED));
 		} else if (length > 28) {
 			text.setStyle(Style.EMPTY.withColor(Formatting.GOLD));
+		} else {
+			switch (state) {
+				case DEFAULT:
+					text.setStyle(Style.EMPTY.withColor(Formatting.GRAY));
+					break;
+				case ON_GROUND:
+					text.setStyle(Style.EMPTY.withColor(Formatting.WHITE));
+					break;
+				case IN_ENTITY:
+					text.setStyle(Style.EMPTY.withColor(Formatting.GREEN));
+					break;
+				case IN_SMALL_WATER:
+					text.setStyle(Style.EMPTY.withColor(Formatting.DARK_AQUA));
+					break;
+				case IN_OPEN_WATER:
+					text.setStyle(Style.EMPTY.withColor(Formatting.AQUA));
+					break;
+				case NOT_EXPOSED:
+					text.setStyle(Style.EMPTY.withColor(Formatting.DARK_AQUA));
+					break;
+				case RAINED_ON:
+					text.setStyle(Style.EMPTY.withColor(Formatting.BLUE));
+					break;
+				case HAS_FISH:
+					text.setStyle(Style.EMPTY.withColor(Formatting.GREEN));
+					break;
+				case NEAR_DESPAWN:
+					text.setStyle(Style.EMPTY.withColor(Formatting.GOLD));
+					break;
+			}
 		}
+
+		if (updateLast) {
+			framesSinceUpdate = 0;
+			lastLength = length;
+		}
+
 		client.player.sendMessage(text, true); 
 	}
 
 	private static String formatDecimalPlaces(double d) {
-		return Double.toString(Math.floor(d*10)/10);
+		String s = Double.toString(Math.floor(d*10)/10);
+		if (s.length() == 3) s = "0"+s;
+		return s;
+	}
+
+	public enum State {
+		DEFAULT,
+		ON_GROUND,
+		IN_ENTITY,
+		IN_SMALL_WATER,
+		IN_OPEN_WATER,
+		NOT_EXPOSED,
+		RAINED_ON,
+		HAS_FISH,
+		NEAR_DESPAWN
 	}
 }
--- src/main/java/com/nettakrim/fishing_ruler/mixin/FishingBobberEntityMixin.java
+++ src/main/java/com/nettakrim/fishing_ruler/mixin/FishingBobberEntityMixin.java
@@ -1,11 +1,16 @@
 package com.nettakrim.fishing_ruler.mixin;
 
 import com.nettakrim.fishing_ruler.FishingRulerClient;
+import com.nettakrim.fishing_ruler.FishingRulerClient.State;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,12 +27,75 @@ public abstract class FishingBobberEntityMixin extends ProjectileEntity {
 	}
 
 	@Shadow abstract public PlayerEntity getPlayerOwner();
+	@Shadow private Entity hookedEntity;
+	@Shadow private int outOfOpenWaterTicks;
+	@Shadow private int hookCountdown;
+	@Shadow abstract public boolean isOpenOrWaterAround(BlockPos pos);
+	@Shadow private boolean caughtFish;
+	@Shadow private int removalTimer;
+
+	private int haveFishCountdown;
+	private int inWaterCountdown;
+	private boolean isOpenWater;
 
 	@Inject(at = @At("HEAD"), method = "tick()V")
 	private void tick(CallbackInfo info) {
 		PlayerEntity owner = getPlayerOwner();
 		if (owner == (PlayerEntity)FishingRulerClient.client.player) {
-			FishingRulerClient.updateText(Math.sqrt(squaredDistanceTo(owner)));
+			FishingRulerClient.updateText(Math.sqrt(squaredDistanceTo(owner)), true, getState());
 		}
+	}
+
+	private State getState() {
+		FishingRulerClient.State state = State.DEFAULT;
+
+		BlockPos blockPos = this.getBlockPos();
+		FluidState fluidState = world.getFluidState(blockPos);
+		if (fluidState.isIn(FluidTags.WATER)) {
+			float f = fluidState.getHeight(world, blockPos);
+			if (f > 0) {
+				inWaterCountdown = 10;
+				isOpenWater = isOpenOrWaterAround(blockPos);
+			}
+		}
+
+		if (inWaterCountdown > 0) {
+			if (isOpenWater) {
+				BlockPos up = blockPos.up();
+				if (world.hasRain(up)) {
+					state = State.RAINED_ON;
+				} else if (!world.isSkyVisible(up) && !world.getFluidState(up).isIn(FluidTags.WATER)) {
+					state = State.NOT_EXPOSED;
+				} else {
+					state = State.IN_OPEN_WATER;
+				}
+			} else {
+				state = State.IN_SMALL_WATER;
+			}
+			inWaterCountdown--;
+		}
+
+		if (onGround) {
+			if (removalTimer >= 1100) {
+				state = State.NEAR_DESPAWN;
+			} else {
+				state = State.ON_GROUND;
+			}
+		}
+
+		if (hookedEntity != null) {
+			state = State.IN_ENTITY;
+		}
+
+		if (caughtFish) {
+			haveFishCountdown = 20;
+		}
+
+		if (haveFishCountdown > 0) {
+			haveFishCountdown--;
+			state = State.HAS_FISH;
+		}
+
+		return state;
 	}
 }
