1d1be85b96d4ac452b8fecaa1a0567355ebfd2b1
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2024-10-27 16:55:08 +0000

    updated to 1.21.2/3

--- src/main/java/com/nettakrim/fishing_ruler/mixin/FishingRodItemMixin.java
+++ src/main/java/com/nettakrim/fishing_ruler/mixin/FishingRodItemMixin.java
@@ -1,5 +1,6 @@
 package com.nettakrim.fishing_ruler.mixin;
 
+import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,15 +10,13 @@ import com.nettakrim.fishing_ruler.FishingRulerClient;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FishingRodItem;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 @Mixin(FishingRodItem.class)
 public class FishingRodItemMixin {
     @Inject(method = "use", at = @At("HEAD"))
-    private void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+    private void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         FishingRulerClient.allowSnapInfer = false;
     }
 }
